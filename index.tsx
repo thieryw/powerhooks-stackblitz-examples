3a3e941ecc3855df656e9ac281cba6a4bac8f372
thieryw <williamalexthiery@hotmail.fr>
2021-02-14 19:03:41 +0100

    useWindowInnerSize example

--- index.tsx
+++ index.tsx
@@ -1,11 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import {useWindowInnerSize} from "powerhooks/useWindowInnerSize";
+
+//this hook triggers an event every time the window changes size.
+//it returns an object with the current window width and height as properties.
 
 const App = ()=>{
+
+  const {windowInnerWidth} = useWindowInnerSize();
+  const colors = ["red", "lightblue", "orange" , "green", "yellow"];
+  
+  
   return(
-    <div>
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: windowInnerWidth < 630 ? "column" : "unset",
+      width: "100vw",
+      height: "100vh"
+      
+      
+    }}>
+      {
+        [1,2,3,4,5].map((buttonNumber, index)=> 
+          <button style={{
+            width: "100px",
+            height: "30px",
+            backgroundColor: `${colors[index]}`,
+            margin: "10px"
+          }} key={buttonNumber}>
+            {buttonNumber}
+          </button>)
+      }
+      
       
     </div>
   )
